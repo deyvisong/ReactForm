a3be7e7b0babc72a19e4f2b91b2757caa0283ebd
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2024-12-14 21:51:45 +0100

    change state inline

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,17 +1,38 @@
 import "./MyForm.css";
+import { useState } from "react";
 
 const MyForm = () => {
+  // 3 Data management
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  console.log(name);
+  console.log(email);
+
   return (
     <div>
       {/* 1 - Form creation */}
       <form>
         <div>
           <label htmlFor="name">Name: </label>
-          <input type="text" name="name" placeholder="Insert your name" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Insert your name"
+            onChange={handleName}
+          />
         </div>
         <label>
           <span>E-mail: </span>
-          <input type="email" name="email" placeholder="Insert your email" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Insert your email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <input className="buttondek" type="submit" value="Register!" />
       </form>
